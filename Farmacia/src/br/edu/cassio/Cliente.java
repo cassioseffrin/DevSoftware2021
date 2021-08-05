6cee0387e1cfd36a8504f652580770c2724fafc0
Cassio Seffrin <cassioseffrin@gmail.com>
2021-08-04 22:06:51 -0300

    exemplo heranca

---
+++ Farmacia/src/br/edu/cassio/Cliente.java
@@ -0,0 +1,16 @@
+package br.edu.cassio;
+
+public class Cliente extends Pessoa{
+	
+	private String telefone;
+	
+	
+	public void setTelefone(String f) {
+		this.telefone = f;
+	}
+	
+	public String getTelefone() {
+		return this.telefone;
+	}
+
+}
--- Farmacia/src/br/edu/cassio/Principal.java
+++ Farmacia/src/br/edu/cassio/Principal.java
@@ -3,9 +3,11 @@ package br.edu.cassio;
 public class Principal {
 	
 	public static void main(String a[]) {
-		Pessoa p = new Pessoa();
-		p.setNome("Maria");
-		System.out.println("Nome: " + p.getNome());
+		Cliente c = new Cliente();
+		c.setNome("Maria");
+		c.setTelefone("3442-4521");
+		System.out.println("Nome: " + c.getNome());
+		System.out.println("Telefone: " + c.getTelefone());
 	}
 
 }
