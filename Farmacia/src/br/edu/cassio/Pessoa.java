19c187c76753abaa7109f599652a96b2bd42774c
Cassio Seffrin <cassioseffrin@gmail.com>
2021-08-04 21:59:29 -0300

    Projeto Farmacia

---
+++ Farmacia/src/br/edu/cassio/Pessoa.java
@@ -0,0 +1,14 @@
+package br.edu.cassio;
+
+public class Pessoa {
+	private String nome;
+	private String cpf;
+	private String rg;
+	
+	public void setNome(String n) {
+		this.nome = n;
+	}
+	public String getNome() {
+		return this.nome;
+	}
+}
---
+++ Farmacia/src/br/edu/cassio/Principal.java
@@ -0,0 +1,11 @@
+package br.edu.cassio;
+
+public class Principal {
+	
+	public static void main(String a[]) {
+		Pessoa p = new Pessoa();
+		p.setNome("Maria");
+		System.out.println("Nome: " + p.getNome());
+	}
+
+}
