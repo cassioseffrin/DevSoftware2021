a1851c733e0f70b8e0d253662514e35766cc8cfa
Cassio Seffrin <cassioseffrin@gmail.com>
2021-08-11 20:35:29 -0300

    gravacao arquivo

--- Farmacia/src/br/edu/cassio/Cliente.java
+++ Farmacia/src/br/edu/cassio/Cliente.java
@@ -1,29 +1,42 @@
 package br.edu.cassio;
 
-public class Cliente extends Pessoa{
-	
-	
-	private static String ARQUIVO = "/Users/cassioseffrin/dev/DevSoftware2021/Farmacia/database/Cliente.csv";
-	
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Cliente extends Pessoa {
+
+	public static final String ARQUIVO = "/Users/cassioseffrin/dev/DevSoftware2021/Farmacia/database/Cliente.csv";
+
 	private String telefone;
-	
-	
+
 	public void setTelefone(String f) {
 		this.telefone = f;
 	}
-	
+
 	public String getTelefone() {
 		return this.telefone;
 	}
 
 	public void salvar() {
-		// TODO Auto-generated method stub
-		
+		File f = new File(this.ARQUIVO);
+		try {
+			FileOutputStream fos = new FileOutputStream(f, true);
+			fos.write(this.toString().getBytes());
+			System.out.println("O objecto cliente foi salvo!");
+			fos.close();
+		} catch (FileNotFoundException fnf) {
+			System.out.println("Caminho nao encontrado");
+		} catch (IOException e) {
+			System.out.println("Erro de gravacao");
+		}
+
 	}
- 
+
 	@Override
 	public String toString() {
-		return super.toString() + ";" + this.telefone;
+		return super.toString() + ";" + this.telefone + "\n";
 	}
 
 }
--- Farmacia/src/br/edu/cassio/Principal.java
+++ Farmacia/src/br/edu/cassio/Principal.java
@@ -1,17 +1,19 @@
 package br.edu.cassio;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Principal {
-	
+
 	public static void main(String a[]) {
-		Cliente c = new Cliente();	
-		c.setNome("Maria");
+		Cliente c = new Cliente();
+		c.setNome("Joao");
 		c.setCpf("234234556");
 		c.setRg("234562");
 		c.setTelefone("3442-4521");
-		c.setEndereco("Rua Tancredo Neves, 524");
-		System.out.println(c); 
+		c.setEndereco("Rua Tancredo Neves, 234");
+		c.salvar();
 	}
 
 }
