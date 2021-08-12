025adb9bae49dc8cd17773455022f31d7aaed28a
Cassio Seffrin <cassioseffrin@gmail.com>
2021-08-11 22:06:30 -0300

    Leitura de clientes

--- Farmacia/src/br/edu/cassio/Cliente.java
+++ Farmacia/src/br/edu/cassio/Cliente.java
@@ -1,15 +1,29 @@
 package br.edu.cassio;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-public class Cliente extends Pessoa {
+public class Cliente extends Pessoa { 
 
 	public static final String ARQUIVO = "/Users/cassioseffrin/dev/DevSoftware2021/Farmacia/database/Cliente.csv";
 
 	private String telefone;
+	
+	
+	public Cliente() {
+	}
+	
+	
+	public Cliente(String nome, String cpf, String rg, String endereco, String telefone) {
+		super(nome, cpf, rg,endereco);
+		this.telefone=telefone;
+	}
 
 	public void setTelefone(String f) {
 		this.telefone = f;
@@ -23,6 +37,7 @@ public class Cliente extends Pessoa {
 		File f = new File(this.ARQUIVO);
 		try {
 			FileOutputStream fos = new FileOutputStream(f, true);
+ 
 			fos.write(this.toString().getBytes());
 			System.out.println("O objecto cliente foi salvo!");
 			fos.close();
@@ -32,6 +47,29 @@ public class Cliente extends Pessoa {
 			System.out.println("Erro de gravacao");
 		}
 
+	}
+	
+	public static List<Cliente> lerTodos() {
+		File f = new File(ARQUIVO);
+		FileInputStream fis;
+		List<Cliente> lstClientes = new ArrayList<Cliente>();
+		try {
+			fis = new FileInputStream(f);
+			Scanner scan = new Scanner(fis);
+			while(scan.hasNext()) {
+				String linha = scan.nextLine();
+				String arr[] = linha.split(";");
+				Cliente cliente = new Cliente(arr[0],arr[1],arr[2],arr[3],arr[4]);
+				lstClientes.add(cliente);	
+			}
+			scan.close();
+			fis.close();
+		} catch (FileNotFoundException fnf) {
+			System.out.println("Caminho nao encontrado");
+		} catch (IOException e) {
+			System.out.println("Erro de gravacao");
+		}
+		return lstClientes;
 	}
 
 	@Override
--- Farmacia/src/br/edu/cassio/Principal.java
+++ Farmacia/src/br/edu/cassio/Principal.java
@@ -2,18 +2,15 @@ package br.edu.cassio;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.List;
 import java.util.Scanner;
 
 public class Principal {
-
 	public static void main(String a[]) {
-		Cliente c = new Cliente();
-		c.setNome("Joao");
-		c.setCpf("234234556");
-		c.setRg("234562");
-		c.setTelefone("3442-4521");
-		c.setEndereco("Rua Tancredo Neves, 234");
-		c.salvar();
+		List<Cliente> lstClientes = Cliente.lerTodos();
+		System.out.println(lstClientes.size());	
+		for (Cliente c : lstClientes) {
+			System.out.println("Nome: " + c.getNome() + " CPF: " +c.getCpf());
+		}
 	}
-
 }
