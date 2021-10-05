4d98a1d75b1af6a42e35ddae5d44dc1f9a34b3c9
Cassio Seffrin <cassioseffrin@gmail.com>
2021-10-04 21:50:06 -0300

    GUI com AWT/Swing

--- Farmacia/src/ui/JanelaPrincipal.java
+++ Farmacia/src/ui/JanelaPrincipal.java
@@ -9,18 +9,12 @@ public class JanelaPrincipal extends JFrame{
 	public JanelaPrincipal(){
 		getContentPane().setLayout(new BorderLayout());
 		getContentPane().add(new PainelTitulo(), BorderLayout.SOUTH);
-		
-		
-
 	}
 	
 	public static void main(String a[]) {
-		
 		JanelaPrincipal j = new JanelaPrincipal();
 		j.setVisible(true);
 		j.setSize(800, 600);
-		
-		
 	}
 	
 
